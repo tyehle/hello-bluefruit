c9cbcd827f07a671406e217f976e4ac51fb07995
Tobin Yehle <tobinyehle@gmail.com>
2020-02-25 12:58:18 -0500

    port the old blinking code over to this project

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,38 @@ extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to c
 // extern crate panic_itm; // logs messages over ITM; requires ITM support
 // extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
 
-use cortex_m::asm;
+extern crate feather_m0 as hal;
+
 use cortex_m_rt::entry;
+use hal::clock::GenericClockController;
+use hal::delay::Delay;
+use hal::pac::{CorePeripherals, Peripherals};
+use hal::prelude::*;
 
 #[entry]
 fn main() -> ! {
-    asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
+    let mut peripherals = Peripherals::take().unwrap();
+    let core = CorePeripherals::take().unwrap();
+    let mut clocks = GenericClockController::with_external_32kosc(
+        peripherals.GCLK,
+        &mut peripherals.PM,
+        &mut peripherals.SYSCTRL,
+        &mut peripherals.NVMCTRL,
+    );
+    let mut pins = hal::Pins::new(peripherals.PORT);
+    let mut red_led = pins.d13.into_open_drain_output(&mut pins.port);
+    let mut delay = Delay::new(core.SYST, &mut clocks);
 
+    let mut delay_time = 200i16;
+    let mut delay_change = 100i16;
     loop {
-        // your code goes here
+        delay.delay_ms(delay_time as u16);
+        red_led.set_high().unwrap();
+        delay.delay_ms(delay_time as u16);
+        red_led.set_low().unwrap();
+        if delay_time <= 100 || delay_time > 1000 {
+            delay_change *= -1;
+        }
+        delay_time += delay_change;
     }
 }
