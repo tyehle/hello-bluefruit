8366b6413c6d51608016846374a2e7219598a3a6
Tobin Yehle <tobinyehle@gmail.com>
2020-04-08 16:55:44 -0700

    write to usb

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,14 @@ extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to c
 // extern crate panic_itm; // logs messages over ITM; requires ITM support
 // extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
 
-extern crate feather_m0 as hal;
+use feather_m0 as hal;
 
-use core::cell::RefCell;
-
+// use core::cell::RefCell;
+// use core::ops::DerefMut;
+use core::fmt::Write as cw;
+use embedded_hal::serial::Write;
 use cortex_m_rt::entry;
-use cortex_m::interrupt::Mutex;
+// use cortex_m::interrupt::Mutex;
 use cortex_m::peripheral::NVIC;
 use hal::gpio::IntoFunction;
 use hal::clock::GenericClockController;
@@ -26,22 +28,8 @@ use usbd_serial::{SerialPort, USB_CLASS_CDC};
 
 
 static mut USB_ALLOCATOR: Option<UsbBusAllocator<UsbBus>> = None;
-static USB_BUS: Mutex<RefCell<Option<UsbDevice<UsbBus>>>> = Mutex::new(RefCell::new(None));
-static USB_SERIAL: Mutex<RefCell<Option<SerialPort<UsbBus>>>> = Mutex::new(RefCell::new(None));
-
-
-// fn configure_usb() {
-
-// }
-
-
-// fn configure_led(pins: &mut hal::Pins) -> impl embedded_hal::digital::v2::OutputPin {
-//     pins.d13.into_open_drain_output(&mut pins.port)
-// }
-
-// fn configure_led(pins: &mut hal::Pins) -> hal::gpio::Pa17<hal::gpio::Output<hal::gpio::OpenDrain>> {
-//     pins.d13.into_open_drain_output(&mut pins.port)
-// }
+static mut USB_BUS: Option<UsbDevice<UsbBus>> = None;
+static mut USB_SERIAL: Option<SerialPort<UsbBus>> = None;
 
 
 #[entry]
@@ -74,29 +62,28 @@ fn main() -> ! {
         USB_ALLOCATOR.as_ref().unwrap()
     };
 
-    cortex_m::interrupt::free(|cs| {
-        // USB_ALLOCATOR.borrow(cs).replace(Some(bus_allocator));
+    red_led.set_high().unwrap();
 
-        let usb_serial = SerialPort::new(&bus_allocator);
-        USB_SERIAL.borrow(cs).replace(Some(usb_serial));
+    unsafe {
+        USB_SERIAL = Some(SerialPort::new(&bus_allocator));
+        // USB_SERIAL.as_ref().unwrap()
+    };
+    // USB_SERIAL.borrow(cs).replace(Some(usb_serial));
 
-        let usb_bus = UsbDeviceBuilder::new(&bus_allocator, UsbVidPid(0x16c0, 0x27dd))
+    unsafe {
+        USB_BUS = Some(UsbDeviceBuilder::new(&bus_allocator, UsbVidPid(0x16c0, 0x27dd))
             .manufacturer("Fake company")
             .product("Serial port")
             .serial_number("TEST")
             .device_class(USB_CLASS_CDC)
-            .build();
-        USB_BUS.borrow(cs).replace(Some(usb_bus));
-    });
+            .build());
+        // USB_BUS.as_ref().unwrap()
+    };
 
     unsafe {
         core.NVIC.set_priority(interrupt::USB, 1);
         NVIC::unmask(interrupt::USB);
     }
-
-
-    // write_usb("Starting up!\n".as_bytes());
-    // write_usb("------------\n".as_bytes());
 
 
     let mut delay_time = 200i16;
@@ -111,54 +98,67 @@ fn main() -> ! {
         }
         delay_time += delay_change;
 
-        // let mut buf = [0; 16];
-        // read_usb(&mut buf);
-        // write_usb("Hello :)\n".as_bytes());
+        test_write();
     }
 }
 
 
-// fn read_usb(buffer: &mut [u8]) -> usize {
-//     unsafe {
-//         match USB_SERIAL.as_mut() {
-//             None => 0,
-//             Some(serial) => serial.read(buffer).unwrap_or(0),
-//         }
-//     }
-// }
-
-
-// fn write_usb(data: &[u8]) -> usize {
-//     unsafe {
-//         match USB_SERIAL.as_mut() {
-//             None => 0,
-//             Some(serial) => serial.write(data).unwrap_or(0),
-//         }
-//     }
-// }
+fn test_write() {
+    cortex_m::interrupt::free(|_| {
+        unsafe {
+            USB_BUS.as_mut().and_then(|bus| {
+                USB_SERIAL.as_mut().map(|serial| {
+                    bus.poll(&mut [serial]);
+                    if serial.dtr() {
+                        let stream = serial as &mut dyn Write<u8, Error=UsbError>;
+                        writeln!(stream, "Hello World").unwrap();
+                    }
+                })
+            });
+        }
+    });
+}
 
 
 #[interrupt]
 fn USB() {
-    cortex_m::interrupt::free(|cs| {
-        use core::ops::DerefMut;
-        use core::fmt::Write as cw;
-        use embedded_hal::serial::Write;
+    cortex_m::interrupt::free(|_| {
+        unsafe {
+            USB_BUS.as_mut().and_then(|bus| {
+                USB_SERIAL.as_mut().map(|serial| {
+                    bus.poll(&mut [serial]);
 
-        if let Some(ref mut bus) = USB_BUS.borrow(cs).borrow_mut().deref_mut() {
-            if let Some(ref mut serial) = USB_SERIAL.borrow(cs).borrow_mut().deref_mut() {
-                bus.poll(&mut [serial]);
+                    let mut buf = [0u8; 64];
 
-                let mut buf = [0u8; 64];
-
-                if let Ok(count) = serial.read(&mut buf) {
-                    let stream = serial as &mut dyn Write<u8, Error=UsbError>;
-                    for (i, c) in buf[..count].iter().enumerate() {
-                        writeln!(stream, "[{}, {}, {}/{}]", *c as char, c, i+1, count).unwrap();
+                    if let Ok(count) = serial.read(&mut buf) {
+                        let stream = serial as &mut dyn Write<u8, Error=UsbError>;
+                        for (i, &c) in buf[..count].iter().enumerate() {
+                            // only 32 to 126 are printable
+                            let printable = if c >= 32 && c <= 126 {c as char} else {' '};
+                            writeln!(stream, "[0x{:x?} ( {} ), {}/{}]", c, printable, i+1, count).unwrap();
+                        }
+                        writeln!(stream, "").unwrap();
                     }
-                    writeln!(stream, "-----").unwrap();
-                }
-            }
+                })
+            });
         }
     });
+
+    // cortex_m::interrupt::free(|cs| {
+    //     if let Some(ref mut bus) = USB_BUS.borrow(cs).borrow_mut().deref_mut() {
+    //         if let Some(ref mut serial) = USB_SERIAL.borrow(cs).borrow_mut().deref_mut() {
+    //             bus.poll(&mut [serial]);
+
+    //             let mut buf = [0u8; 64];
+
+    //             if let Ok(count) = serial.read(&mut buf) {
+    //                 let stream = serial as &mut dyn Write<u8, Error=UsbError>;
+    //                 for (i, c) in buf[..count].iter().enumerate() {
+    //                     writeln!(stream, "[{}, {}, {}/{}]", *c as char, c, i+1, count);
+    //                 }
+    //                 writeln!(stream, "-----");
+    //             }
+    //         }
+    //     }
+    // });
 }
